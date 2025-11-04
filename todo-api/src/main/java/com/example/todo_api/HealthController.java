3a3c558c8fcd7597504328492117ef8e463178c2
hukum <test@example.com>
2025-11-04 21:34:37 +0900

    2.19 完了

--- todo-api/src/main/java/com/example/todo_api/HealthController.java
+++ todo-api/src/main/java/com/example/todo_api/HealthController.java
@@ -1,7 +1,16 @@
 package com.example.todo_api;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.todo_api.controller.HealthApi;
 
+@RestController
 public class HealthController implements HealthApi{
 
+    @Override
+    public ResponseEntity<Void> healthGet(){
+        return ResponseEntity.ok().build();
+        }
+    
 }
